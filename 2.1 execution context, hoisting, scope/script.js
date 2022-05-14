6429a8f7ed2c217b755229e915761e646fbc06b3
mdwisu <dwisusanto784@gmail.com>
2022-05-14 20:47:44 +0700

    materi 2

---
+++ 2.1 execution context, hoisting, scope/script.js
@@ -0,0 +1,25 @@
+// execution phase
+
+
+// let nama = 'dwi';
+// let umur = 33;
+// console.log(sayHello());
+
+
+// function sayHello() { 
+//     return `Halo, nama saya ${nama}, saya ${umur} tahun.`
+//  }
+
+//  local execution context
+
+// let nama = 'dwi';
+// let username = '@dwi';
+
+// function cetakURL(username) {
+//     let instagramURL = 'https://instagram.com/';
+//     return instagramURL + username;
+// }
+
+// console.log(cetakURL(username));
+
+
