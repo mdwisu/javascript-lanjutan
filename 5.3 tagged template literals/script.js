a70fdda3634fdae5e47b6039243f2e8f607b0e9e
mdwisu <dwisusanto784@gmail.com>
2022-05-25 01:26:08 +0700

    tagged templates

--- 5.3 tagged template literals/script.js
+++ 5.3 tagged template literals/script.js
@@ -1,11 +1,31 @@
 // tagged Templates
+// const nama = 'Dwi';
+// const umur = 21;
+
+// function coba(strings, ...values) {
+//   // let result = '';
+//   // strings.forEach((str, i) => {
+//   //   result += `${str}${values[i] || ''}`
+//   // });
+//   // return result;
+
+//   return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}`, '');
+  
+// }
+
+// const str = coba`Halo, nama saya ${nama}, saya ${umur} tahun.`;
+
+// console.log(str);
+
 const nama = 'Dwi';
 const umur = 21;
+const email = 'dwisusanto784@gmail.com';
 
-function coba(strings, ...values) {
-  return nama;
+function highlight(strings, ...values) {
+  return strings.reduce((result, str, i) => `${result}${str}<span class="hl">${values[i] || ''}</span>`, '');
+  
 }
 
-const str = coba`Halo, nama saya ${nama}, saya ${umur} tahun`;
+const str = highlight`Halo, nama saya ${nama}, saya ${umur} tahun, dan email. saya adalah : ${email}`;
 
-console.log(str);
+document.body.innerHTML = str;
